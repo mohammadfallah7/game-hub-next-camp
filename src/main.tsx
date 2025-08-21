9678581e95295316bd6d4c038a90416373a09f2f
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-08-21 11:56:08 +0330

    feat: :sparkles: Cconfig react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
