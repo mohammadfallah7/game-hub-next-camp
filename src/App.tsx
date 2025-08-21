c11c098e2a7876338ad6935d82ebb7bb0f1b0c05
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-08-21 11:22:43 +0330

    feat: :sparkles: Implement header & mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Grid templateColumns={{ base: "1fr", md: "repeat(12, 1fr)" }} gap={5}>
-      <GridItem colSpan={12} border="1px solid" as="header">
-        Header
+      <GridItem colSpan={12} as="header">
+        <Header />
       </GridItem>
-      <GridItem
-        display={{ base: "none", md: "block" }}
-        colSpan={3}
-        border="1px solid"
-        as="aside"
-      >
+      <GridItem display={{ base: "none", md: "block" }} colSpan={3} as="aside">
         Sidebar
       </GridItem>
-      <GridItem colSpan={{ base: 12, md: 9 }} border="1px solid" as="aside">
+      <GridItem colSpan={{ base: 12, md: 9 }} as="aside">
         Games
       </GridItem>
     </Grid>
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import {
+  HStack,
+  Image,
+  Input,
+  Group,
+  IconButton,
+  Icon,
+} from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+import { ColorModeButton } from "./ui/color-mode";
+
+const Header = () => {
+  return (
+    <HStack gap={5} py={3} pl={3} pr={5}>
+      <Image src="/logo.webp" alt="Logo" width={16} />
+      <Group attached w="full" mr={2}>
+        <Input placeholder="Search games..." />
+        <IconButton>
+          <Icon size="sm">
+            <FaSearch />
+          </Icon>
+        </IconButton>
+      </Group>
+      <ColorModeButton />
+    </HStack>
+  );
+};
+
+export default Header;
