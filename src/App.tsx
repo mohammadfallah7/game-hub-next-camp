55bf72e42d1dcc19d631a9bc70d2157107036c4a
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-08-21 10:18:33 +0330

    feat: :sparkles: Create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { Button, Heading } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Heading size="7xl">Game Hub</Heading>
-      <Button size="2xl" variant="surface">
-        Click Me!
-      </Button>
-    </div>
+    <Grid templateColumns={{ base: "1fr", md: "repeat(12, 1fr)" }} gap={5}>
+      <GridItem colSpan={12} border="1px solid" as="header">
+        Header
+      </GridItem>
+      <GridItem
+        display={{ base: "none", md: "block" }}
+        colSpan={3}
+        border="1px solid"
+        as="aside"
+      >
+        Sidebar
+      </GridItem>
+      <GridItem colSpan={{ base: 12, md: 9 }} border="1px solid" as="aside">
+        Games
+      </GridItem>
+    </Grid>
   );
 };
 
