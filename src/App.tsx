d19d3fd24c9a4f987e9aaead8103328895747eea
Mohammad_Fallah <mohammadfallah.w@gmail.com>
2025-08-21 10:00:40 +0330

    feat: :sparkles: Config chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Button, Heading } from "@chakra-ui/react";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Heading size="7xl">Game Hub</Heading>
+      <Button size="2xl" variant="surface">
+        Click Me!
+      </Button>
+    </div>
+  );
 };
 
 export default App;
